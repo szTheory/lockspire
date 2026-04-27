10653fc65a959de37779ba6cd4b493a4c6b9a926
szTheory <szTheory@users.noreply.github.com>
2026-04-27 17:15:54 -0400

    test(30-02): add failing test for device code generation

---
+++ test/lockspire/security/device_code_test.exs
@@ -0,0 +1,28 @@
+defmodule Lockspire.Security.DeviceCodeTest do
+  use ExUnit.Case, async: true
+
+  alias Lockspire.Security.DeviceCode
+
+  describe "generate_user_code/0" do
+    test "returns an 8-character string" do
+      code = DeviceCode.generate_user_code()
+      assert String.length(code) == 8
+    end
+
+    test "composed only of characters from BCDFGHJKLMNPQRSTVWXZ" do
+      code = DeviceCode.generate_user_code()
+      assert Regex.match?(~r/^[BCDFGHJKLMNPQRSTVWXZ]+$/, code)
+    end
+  end
+
+  describe "generate_device_code/0" do
+    test "returns a high entropy string" do
+      code1 = DeviceCode.generate_device_code()
+      code2 = DeviceCode.generate_device_code()
+
+      assert is_binary(code1)
+      assert String.length(code1) >= 43 # 32 bytes base64 encoded is at least 43 chars
+      assert code1 != code2
+    end
+  end
+end
