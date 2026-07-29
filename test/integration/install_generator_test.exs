6767a3c9a445e80e2d20bdc14fba2b5097d44c90
szTheory <szTheory@users.noreply.github.com>
2026-07-29 08:56:38 -0400

    style(127-01): satisfy mix format on the manifest-version assertion
    Redundant parens around the pipe expression tripped
    mix format --check-formatted, which mix qa runs.

--- test/integration/install_generator_test.exs
+++ test/integration/install_generator_test.exs
@@ -43,7 +43,7 @@ defmodule Lockspire.InstallGeneratorTest do
 
     manifest = load_manifest!()
 
-    assert manifest["version"] == (Application.spec(:lockspire, :vsn) |> List.to_string())
+    assert manifest["version"] == Application.spec(:lockspire, :vsn) |> List.to_string()
     assert manifest["version"] =~ ~r/^\d+\.\d+\.\d+/
     assert manifest["inputs"]["mount_path"] == "/lockspire"
     assert manifest["inputs"]["storage_prefix"] == "lockspire"
